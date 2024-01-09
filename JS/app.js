02b7d31c41debd667ed93e8337bc29735406048a
Miguel-Hernandez-Garcia <mighergar@gmail.com>
2024-01-09 16:54:37 +0100

    Iteration1

---
+++ JS/app.js
@@ -0,0 +1,38 @@
+/* EJERCICIOS DE BASIC ALGORITHMS DE JAVASCRIPT
+1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
+1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+*/
+
+//!---------------------------------------------------------------------------
+//? 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+//!---------------------------------------------------------------------------
+
+let myFavoriteHero;
+myFavoriteHero = "Hulk";
+
+//!---------------------------------------------------------------------------
+//? 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+//!---------------------------------------------------------------------------
+
+let x;
+x = 50;
+
+//!---------------------------------------------------------------------------
+//? 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
+//!---------------------------------------------------------------------------
+
+let h;
+h = 5;
+
+let y;
+y = 10;
+
+//!---------------------------------------------------------------------------
+//? 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+//!---------------------------------------------------------------------------
+
+let z;
+z = h + y;
+
