c24cf1804aa7ebd99f0ea1e83c4485d2d859be70
Miguel-Hernandez-Garcia <mighergar@gmail.com>
2024-01-10 16:49:02 +0100

    Iteration#3

--- JS/basicAlgorithms/app.js
+++ JS/basicAlgorithms/app.js
@@ -87,3 +87,48 @@ const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
 car1.finalPrice = car1.basePrice + globalBasePrice;
 car2.finalPrice = car2.basePrice + globalBasePrice;
 console.log(car2);
+
+/* EJERCICIOS DE BASIC ALGORITHMS DE JAVASCRIPT
+Iteracion 3: Variables operadores*/
+
+/*---------------------------------------------------------------------------
+//? 3.1 Multiplica 10 por 5 y muestra el resultado mediante console.
+-----------------------------------------------------------------------------*/
+
+let Multiplica = 10;
+Multiplica *=5;
+console.log(Multiplica);
+
+/*---------------------------------------------------------------------------
+//? 3.2 Divide 10 por 2 y muestra el resultado en un console.
+-----------------------------------------------------------------------------*/
+
+let Divide = 10;
+Divide /= 2;
+console.log(Divide);
+
+/*---------------------------------------------------------------------------
+//? 3.3 Muestra mediante un console el resto de dividir 15 por 9.
+-----------------------------------------------------------------------------*/
+
+let resto = 15;
+resto %=9;
+console.log(resto);
+
+/*------------------------------------------------------------------------------------------------------------
+//? 3.4 Usa el correcto operador de asignación que resultará en o = 15, teniendo dos variables p = 10 y j = 5.
+------------------------------------------------------------------------------------------------------------*/
+
+let p = 10;
+let j = 5;
+let o = p + j;
+console.log(o);
+
+/*------------------------------------------------------------------------------------------------------------
+//? 3.5 Usa el correcto operador de asignación que resultará en i = 50,teniendo dos variables c = 10 y m = 5.
+------------------------------------------------------------------------------------------------------------*/
+
+let c = 10;
+let m = 5;
+let i = c*m;
+console.log(i);
