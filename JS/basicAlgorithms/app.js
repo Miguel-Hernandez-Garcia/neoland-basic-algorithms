c9f1cefa710466b7fd2cb3bbb16477aa5995519e
Miguel-Hernandez-Garcia <mighergar@gmail.com>
2024-01-10 11:24:06 +0100

    Cambiados los comentarios

--- JS/basicAlgorithms/app.js
+++ JS/basicAlgorithms/app.js
@@ -1,8 +1,5 @@
 /* EJERCICIOS DE BASIC ALGORITHMS DE JAVASCRIPT
-1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
-1.2 Crea una variable llamada x, asigna el valor 50 a ella.
-1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10. 
-1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+Iteracion 1: Variables
 */
 
 //!---------------------------------------------------------------------------
@@ -19,9 +16,9 @@ myFavoriteHero = "Hulk";
 let x;
 x = 50;
 
-//!---------------------------------------------------------------------------
+//!----------------------------------------------------------------------------
 //? 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
-//!---------------------------------------------------------------------------
+//!----------------------------------------------------------------------------
 
 let h;
 h = 5;
